8f11c16324b54a039efa398e44edb4469b878c6b
robkemp <rjameskemp@gmail.com>
2016-12-15 15:17:49 -0700

    new data and updated download functions

--- server.R
+++ server.R
@@ -1,7 +1,89 @@
-
+library(dplyr)
+library(tidyr)
 library(plotly)
+library(scales)
 
-source("setup.R")
+
+county_choices=read.csv("county_names.csv", stringsAsFactors = FALSE)%>%
+  select(county)
+
+all_data= read.csv("NetMigrationByAgeComparison.csv", stringsAsFactors = FALSE)
+
+#### Net Migration by Age Graph and Data #####
+
+## Generates a Plotly Chart
+mig_age_p=function(fips){
+  
+  data=all_data%>%    
+    mutate(FIPS=as.numeric(FIPS))%>%
+    filter(FIPS==fips)
+  
+  p=plot_ly(data=data, x= ~FiveYearAgeGroups, y=~NetMig9500, type="scatter", line=list(color = "rgb(31,74,126)"), name="1995 to 2000")
+    p=add_trace(p,x=~FiveYearAgeGroups, y=~NetMig0010, line=list(color="rgb(239,117,33)", width=2.5, dash="dash"), name= "2000 to 2010")
+    p=add_trace(p,x=~FiveYearAgeGroups, y=~NetMig1020, line=list(color="rgb(0,149,58)", width=2.5, dash="dot"), name= "2010 to 2020")
+    p=layout(p,
+      barmode="stacked",
+      title="Net Migration by Age - Net Migrants",
+      xaxis=list(
+        title=" Age"),
+      yaxis=list(
+        title=" Net Migration"),
+      margin=list(t=60)
+    )
+  p
+}
+
+## Generates the data download
+mig_age_d=function(fips){
+  
+  x=all_data%>%
+    mutate(FIPS=as.numeric(FIPS))%>%
+    filter(FIPS==fips)%>%
+    # bind_cols(data.frame(County=rep(name, 90)))%>%
+    select(County=County.name, AgeGroup=FiveYearAgeGroups, NetMig9500, NetMig0010, NetMig1020)
+  
+  
+  return(x)
+}
+
+#### Share of Net Migration by Age Graph and Data #####
+
+## Generates a Plotly Chart
+mig_share_p=function(fips){
+  
+  data=all_data%>%    
+    mutate(FIPS=as.numeric(FIPS))%>%
+    filter(FIPS==fips)
+  
+  p= plot_ly(data, x=~FiveYearAgeGroups, y=~1000*as.numeric(Rate9500), type="scatter",line=list(color = "rgb(31,74,126)"), name="1995 to 2000")
+  p=  add_trace(p,data, x=~FiveYearAgeGroups, y=~1000*as.numeric(Rate0010),  line=list(color="rgb(239,117,33)", width=2.5, dash="dash"), name= "2000 to 2010")
+  p=  add_trace(p,data, x=~FiveYearAgeGroups, y=~1000*as.numeric(Rate1020),  line=list(color="rgb(0,149,58)", width=2.5, dash="dot"), name= "2010 to 2020")
+  p=  layout(p,
+      title="Net Migration by Age - Rates", 
+      xaxis=list(
+        title=" Age"),
+      yaxis=list(
+        title=" Net Migration Rate (per 1,000 population)"),
+      margin=list(t=60)
+    )
+  p
+}
+
+## Generates the data download
+mig_share_d=function(fips){
+  
+  x=all_data%>%
+    mutate(FIPS=as.numeric(FIPS))%>%
+    filter(FIPS==fips)%>%
+    # bind_cols(data.frame(County=rep(name, 90)))%>%
+    select(County=County.name, AgeGroup=FiveYearAgeGroups, Rate9500, Rate0010, Rate1020)
+  
+  
+  return(x)
+}
+
+# 
+# source("setup.R")
 
 function(input, output, session) {
 
@@ -13,7 +95,7 @@ county=reactive({filter(read.csv("county_names.csv"), county==input$county)%>%
   
   output$netMigAge=renderPlotly({mig_age_p(county())})
   
-  nm_data=reactive({mig_age_d(county(), input$county)})
+  nm_data=reactive({mig_age_d(county())})
   
   output$netMigAgeData=downloadHandler(
     filename= function(){
@@ -31,7 +113,7 @@ county=reactive({filter(read.csv("county_names.csv"), county==input$county)%>%
 
 output$netMigAgeShare=renderPlotly({mig_share_p(county())})
 
-nm__share_data=reactive({mig_share_d(county(), input$county)})
+nm__share_data=reactive({mig_share_d(county())})
 
 output$netMigAgeDataShare=downloadHandler(
   filename= function(){
